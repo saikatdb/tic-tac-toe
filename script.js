74aca1aea180dbd02c7d696de7549cec9d6c34d6
Saikat Debnath <112309999+saikatdb@users.noreply.github>
2023-04-02 18:34:25 +0530

    Add gameBoard module

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log('Hello World');
+const gameBoard = (() => {
+  const board = ['x', 'x', 'x', 'o', 'o', 'o', 'x', 'x', 'x'];
+  const cells = document.getElementsByClassName('cell');
+
+  const printBoard = () => {
+    for (let i = 0; i < cells.length; i++) {
+      cells[i].textContent = board[i];
+    }
+  };
+
+  return { printBoard };
+})();
+
+gameBoard.printBoard();
+
+const players = (name) => {
+  let playerMark = '';
+  const chooseMark = (choice) => {
+    if (choice === 'x') {
+      playerMark = 'X';
+      // return playerMark;
+    } else if (choice === 'o') {
+      playerMark = 'O';
+      // return playerMark;
+    }
+    return playerMark;
+  };
+
+  return { name, chooseMark };
+};
