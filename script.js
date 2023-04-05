f78d7a8f0d8e9cc02a596037517021c9a4375ec2
Saikat Debnath <112309999+saikatdb@users.noreply.github>
2023-04-05 11:55:15 +0530

    Add win and draw logic

--- script.js
+++ script.js
@@ -14,9 +14,6 @@ const gameBoard = (() => {
 gameBoard.printBoard();
 
 const players = (name, mark) => {
-  // const cellsNode = document.querySelectorAll('.cell');
-  // this.mark = mark;
-
   const getMark = () => mark;
   const getName = () => name;
 
@@ -26,6 +23,7 @@ const players = (name, mark) => {
 const playerOne = players('Player 1', 'X');
 const playerTwo = players('Player 2', 'O');
 
+// To display the game
 const displayController = (() => {
   const container = document.querySelector('.container');
   const boardNode = document.querySelector('.game-board');
@@ -38,8 +36,15 @@ const displayController = (() => {
   // playerTurn to change player name after turn
   let playerTurn = playerOne.getName();
 
+  //
+
+  // to stop the round
+  const cancelled = false;
+
   const round = () => {
     const cellsNode = document.querySelectorAll('.cell');
+
+    let roundNum = 0;
 
     // to show player name on ui
     divPlayerTurn.textContent = `${playerTurn}'s turn`;
@@ -49,8 +54,16 @@ const displayController = (() => {
       cell.addEventListener('click', () => {
         // '!cell.childNodes.length' will see if the cell is empty or not
         if (!cell.childNodes.length) {
+          // To stop round from happening in win/lose
+          if (displayController.cancelled === true) {
+            return;
+          }
+
           gameBoard.board.splice(cell.dataset.index, 1, playerMark);
           gameBoard.printBoard();
+          gameController.winCon();
+          roundNum += 1;
+          console.log(roundNum);
 
           // change mark after each turn
           playerMark =
@@ -68,27 +81,75 @@ const displayController = (() => {
           // change name in the ui
           divPlayerTurn.textContent = `${playerTurn}'s turn`;
           container.insertBefore(divPlayerTurn, boardNode);
-        } else {
-          console.log('yo');
+
+          // to execute if draws
+          if (roundNum === 9) {
+            container.removeChild(divPlayerTurn);
+            divPlayerTurn.textContent = `It's a draw!`;
+            container.insertBefore(divPlayerTurn, boardNode);
+          }
+
+          if (displayController.cancelled === true) {
+            container.removeChild(divPlayerTurn);
+          }
         }
+        // else {
+        // }
       });
     });
   };
 
-  // const winCon = () => {
-  //   const winArray = [
-  //     [0, 1, 2],
-  //     [3, 4, 5],
-  //     [6, 7, 8],
-  //     [0, 3, 6],
-  //     [1, 4, 7],
-  //     [2, 5, 8],
-  //     [0, 4, 8],
-  //     [2, 4, 6],
-  //   ];
-  // };
+  return { round, cancelled };
+})();
 
-  return { round };
+const gameController = (() => {
+  const container = document.querySelector('.container');
+  const boardNode = document.querySelector('.game-board');
+  const divPlayerWins = document.createElement('div');
+  divPlayerWins.setAttribute('class', 'wins');
+
+  const winCon = () => {
+    const winArray = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    const checkTrue = (element) => element === true;
+    for (let i = 0; i < winArray.length; i += 1) {
+      const winX = [];
+      const winO = [];
+
+      for (let j = 0; j < winArray[i].length; j += 1) {
+        winX[j] = gameBoard.board[winArray[i][j]].includes('X');
+        winO[j] = gameBoard.board[winArray[i][j]].includes('O');
+      }
+      // console.log(winX);
+      const winnerX = winX.every(checkTrue);
+      const winnerO = winO.every(checkTrue);
+
+      if (winnerX) {
+        console.log('hey');
+        displayController.cancelled = true;
+
+        divPlayerWins.textContent = `Congratulation! ${playerOne.getName()} has won!`;
+        container.insertBefore(divPlayerWins, boardNode);
+      }
+      if (winnerO) {
+        console.log('hey');
+        displayController.cancelled = true;
+
+        divPlayerWins.textContent = `Congratulation! ${playerTwo.getName()} has won!`;
+        container.insertBefore(divPlayerWins, boardNode);
+      }
+    }
+  };
+
+  return { winCon };
 })();
 
 displayController.round();
